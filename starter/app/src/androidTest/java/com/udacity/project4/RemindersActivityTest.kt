82ea5a943b1e4bc3ac41d8a8a2edcd9642376a16
Daniil <porvipop@mail.ru>
2022-03-01 08:55:42 +0300

    finishing touches

--- starter/app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
+++ starter/app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
@@ -4,12 +4,12 @@ import android.app.Activity
 import android.app.Application
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
-import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.IdlingRegistry
-import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.matcher.RootMatchers
-import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.RootMatchers.withDecorView
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.udacity.project4.locationreminders.RemindersActivity
@@ -20,8 +20,10 @@ import com.udacity.project4.locationreminders.reminderslist.RemindersListViewMod
 import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
 import com.udacity.project4.util.DataBindingIdlingResource
 import com.udacity.project4.util.monitorActivity
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
-import org.hamcrest.CoreMatchers
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.not
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -35,15 +37,22 @@ import org.koin.test.get
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
-//END TO END test to black box test the app
+// END TO END test to black box test the app
 class RemindersActivityTest :
-    AutoCloseKoinTest() {// Extended Koin Test - embed autoclose @after method to close Koin after every test
+    AutoCloseKoinTest() { // Extended Koin Test - embed autoclose @after method to close Koin after every test
 
     private lateinit var repository: ReminderDataSource
     private lateinit var appContext: Application
-
-    // An idling resource that waits for Data Binding to have no pending bindings.
     private val dataBindingIdlingResource = DataBindingIdlingResource()
+
+    // get activity context
+    private fun getActivity(activityScenario: ActivityScenario<RemindersActivity>): Activity? {
+        var activity: Activity? = null
+        activityScenario.onActivity {
+            activity = it
+        }
+        return activity
+    }
 
     /**
      * As we use Koin as a Service Locator Library to develop our code, we'll also use Koin to test our code.
@@ -51,7 +60,7 @@ class RemindersActivityTest :
      */
     @Before
     fun init() {
-        stopKoin()//stop the original app koin
+        stopKoin() // stop the original app koin
         appContext = getApplicationContext()
         val myModule = module {
             viewModel {
@@ -69,85 +78,80 @@ class RemindersActivityTest :
             single { RemindersLocalRepository(get()) as ReminderDataSource }
             single { LocalDB.createRemindersDao(appContext) }
         }
-        //declare a new koin module
+        // declare a new koin module
         startKoin {
             modules(listOf(myModule))
         }
-        //Get our real repository
+        // Get our real repository
         repository = get()
 
-        //clear the data to start fresh
+        // clear the data to start fresh
         runBlocking {
             repository.deleteAllReminders()
         }
     }
-
 
     @Before
     fun registerIdlingResource() {
         IdlingRegistry.getInstance().register(dataBindingIdlingResource)
     }
 
-    /**
-     * Unregister your Idling Resource so it can be garbage collected and does not leak any memory.
-     */
     @After
     fun unregisterIdlingResource() {
         IdlingRegistry.getInstance().unregister(dataBindingIdlingResource)
     }
 
     @Test
-    fun testErrorEnterTitleSnackBar() {
+    fun addNewReminder_ReminderListShowsNewReminderAndToast() = runBlocking {
+
+        // GIVEN - an empty ReminderList
         val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
         dataBindingIdlingResource.monitorActivity(activityScenario)
+        val activity = getActivity(activityScenario)
 
-        Espresso.onView(ViewMatchers.withId(R.id.addReminderFAB)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.saveReminder)).perform(ViewActions.click())
+        // WHEN - new Reminder added
+        onView(withId(R.id.addReminderFAB)).perform(click())
+        onView(withId(R.id.reminderTitle)).perform(replaceText("Title"))
+        onView(withId(R.id.reminderDescription)).perform(replaceText("Description"))
+        onView(withId(R.id.selectLocation)).perform(click())
+        onView(withId(R.id.map)).perform(longClick())
+        onView(withId(R.id.button)).perform(click())
+        onView(withId(R.id.saveReminder)).perform(click())
 
-        val snackBarMessage = appContext.getString(R.string.err_enter_title)
-        Espresso.onView(ViewMatchers.withText(snackBarMessage))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        // THEN - ReminderList shows item and toast
+        // ToastCheck from https://knowledge.udacity.com/questions/663647
+        onView(withText(R.string.reminder_saved)).inRoot(withDecorView(not(`is`(activity?.window?.decorView))))
+            .check(
+                matches(
+                    isDisplayed()
+                )
+            )
+        onView(withText("Title")).check(matches(isDisplayed()))
 
-        activityScenario.close()
+        runBlocking {
+            delay(3000)
+        }
     }
 
     @Test
-    fun testReminderSavedToastMessage() {
+    fun addNewReminderWithoutLocation_ShowsSnackbar() = runBlocking {
+
         val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
         dataBindingIdlingResource.monitorActivity(activityScenario)
 
-        Espresso.onView(ViewMatchers.withId(R.id.addReminderFAB)).perform(ViewActions.click())
+        // GIVEN - an SaveReminder Screen without Selected Location
+        onView(withId(R.id.addReminderFAB)).perform(click())
+        onView(withId(R.id.reminderTitle)).perform(replaceText("Title"))
+        onView(withId(R.id.reminderDescription)).perform(replaceText("Description"))
 
-        Espresso.onView(ViewMatchers.withId(R.id.reminderTitle))
-            .perform(ViewActions.replaceText("Title"))
-        Espresso.onView(ViewMatchers.withId(R.id.reminderDescription))
-            .perform(ViewActions.replaceText("Desc"))
-        Espresso.onView(ViewMatchers.withId(R.id.selectLocation)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.button)).perform(ViewActions.click())
+        // WHEN - click saveButton
+        onView(withId(R.id.saveReminder)).perform(click())
 
-        Espresso.onView(ViewMatchers.withId(R.id.saveReminder)).perform(ViewActions.click())
+        // THEN - shows Snackbar
+        onView(withId(com.google.android.material.R.id.snackbar_text)).check(matches(isDisplayed()))
 
-        Espresso.onView(ViewMatchers.withText(R.string.reminder_saved)).inRoot(
-            RootMatchers.withDecorView(
-                CoreMatchers.not(
-                    CoreMatchers.`is`(
-                        getActivity(activityScenario).window.decorView
-                    )
-                )
-            )
-        )
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-
-        activityScenario.close()
-    }
-
-    // get activity context
-    private fun getActivity(activityScenario: ActivityScenario<RemindersActivity>): Activity {
-        lateinit var activity: Activity
-        activityScenario.onActivity {
-            activity = it
+        runBlocking {
+            delay(3000)
         }
-        return activity
     }
-
 }
